19bff7fd0a696d36003e316abda3da1ca73fd908
Akshay Thakur <akshaythakur@berkeley.edu>
2024-06-23 11:03:54 -0700

    add rules modal

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,36 +1,43 @@
 "use client";
 
 import { useGameSettings } from "@/context/GameSettingsContext";
+import { useState } from "react";
+import { RulesModal } from "./RulesModal";
 
 export const Landing = () => {
 	const gameSettings = useGameSettings();
 	if (!gameSettings) throw new Error("Game state must be defined");
 
+	const [rulesOpen, setRulesOpen] = useState(false);
+
 	return (
-		<section>
-			<div className="flex justify-center">
-				<div className="flex-col justify-center">
-					<h3 className="max-w-sm">
-						Think you know fashion? Guess and find out...
-					</h3>
-					<div className="flex justify-center space-x-4 w-full">
-						<button
-							onClick={() => {}}
-							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-						>
-							?
-						</button>
-						<button
-							onClick={() => {
-								gameSettings.setSegment("GUESS");
-							}}
-							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-						>
-							Play
-						</button>
+		<>
+			{rulesOpen && <RulesModal isOpen={rulesOpen} setIsOpen={setRulesOpen} />}
+			<section>
+				<div className="flex justify-center">
+					<div className="flex-col justify-center">
+						<h3 className="max-w-sm">
+							Think you know fashion? Guess and find out...
+						</h3>
+						<div className="flex justify-center space-x-4 w-full">
+							<button
+								onClick={() => setRulesOpen(true)}
+								className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+							>
+								?
+							</button>
+							<button
+								onClick={() => {
+									gameSettings.setSegment("GUESS");
+								}}
+								className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+							>
+								Play
+							</button>
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</>
 	);
 };
--- src/components/RulesModal.tsx
+++ src/components/RulesModal.tsx
@@ -1,3 +1,48 @@
-export const Timer = () => {
-	return <section>Timer</section>;
+"use client";
+
+import {
+	Description,
+	Dialog,
+	DialogBackdrop,
+	DialogPanel,
+	DialogTitle,
+} from "@headlessui/react";
+import { Dispatch, SetStateAction } from "react";
+
+interface RulesModalProps {
+	isOpen: boolean;
+	setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const RulesModal = (props: RulesModalProps) => {
+	const { isOpen, setIsOpen } = props;
+	return (
+		<Dialog
+			open={isOpen}
+			onClose={() => setIsOpen(false)}
+			className="relative z-50"
+		>
+			<DialogBackdrop className="fixed inset-0 bg-black/30" />
+			<div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+				<DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
+					<DialogTitle className="font-bold">How to Play</DialogTitle>
+					<Description>
+						You'll be shown 10 random images from high fashion runway shows. Try
+						to guess the brand, season, and year as fast as possible to maximize
+						your points!
+					</Description>
+					<p>
+						1000 points is a perfect score. Each correct brand gives you 60
+						points, each correct season gives you 10 points, and each correct
+						year gives you 30 points. You'll have a minute to make guesses for
+						each runway image, and the slower you are the more points will be
+						deducted from your score.
+					</p>
+					<div className="flex gap-4">
+						<button onClick={() => setIsOpen(false)}>Close</button>
+					</div>
+				</DialogPanel>
+			</div>
+		</Dialog>
+	);
 };
